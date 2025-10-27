79436bb650f192d1296f24e0de86141089e7afb4
Joaquin-App4U <jespallargasl@uoc.edu>
2025-10-27 18:58:15 +0100

    Update Controlador.java

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -1,3 +1,29 @@
 public class Controlador {
+//(metodos) Gestión articulos
+
+
+
+  
+    System.out.print("Descripción: "); String descripcion = sc.nextLine();
+        double precio = leerDouble("Precio venta: ", 0, Double.MAX_VALUE);
+        double envio = leerDouble("Gastos de envío: ", 0, Double.MAX_VALUE);
+        int tiempo = leerInt("Tiempo preparación (minutos): ", 1, Integer.MAX_VALUE);
+
+        articulos.add(new Articulo(codigo, descripcion, precio, envio, tiempo));
+        System.out.println("Artículo añadido correctamente.");
+    }
+
+    private static void mostrarArticulos() {
+        if (articulos.isEmpty()) {
+            System.out.println("No hay artículos registrados.");
+            return;
+        }
+        System.out.println("--- LISTADO DE ARTÍCULOS ---");
+        for (Articulo a : articulos) {
+            System.out.println(a);
+        }
+    }
+
+
 
 }
